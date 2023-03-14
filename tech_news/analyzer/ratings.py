a5fcc6c0c8a9724cae6b16228aa8927a23f2b89e
Aluisio <aluisioordones1@gmail.com>
2023-03-14 17:25:40 -0300

    requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,18 @@
-# Requisito 10
+from tech_news.database import find_news
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories_amount = dict()
+
+    for new in find_news():
+        category = new["category"]
+        if not categories_amount.get(category):
+            categories_amount[category] = 1
+        else:
+            categories_amount[category] += 1
+
+    alphabet_order = sorted(categories_amount.items(), key=lambda t: t[0])
+
+    numeric_order = sorted(alphabet_order, key=lambda t: t[1], reverse=True)
+
+    return [category for category, _ in numeric_order]
